eff3fbddf04f3d1dee4fc7632d83819c4dd35b1a
Ola Tangen Kulseng <otkulseng@Ola-sin-Mac-mini.local>
2025-02-05 12:48:12 +0100

    order parameter apply

--- kpm/bdg.py
+++ kpm/bdg.py
@@ -27,11 +27,14 @@ def bdg_base_matrix(indices: torch.Tensor, blocks: torch.Tensor):
     return BlockSparseMatrix(indices, blocks)
 
 def bdg_order_matrix_indices(indices: torch.Tensor):
+    row = indices[:, 0]
+    col = indices[:, 1]
+
     row = torch.concat([2*row, 2*row + 1])
     col = torch.concat([2*col + 1, 2*col])
 
     # First regular, then conjugated
-    return torch.vstack([row, col])
+    return torch.vstack([row, col]).T
 
 def apply_order_parameters(indices: torch.Tensor, order_parameters: torch.Tensor, V: torch.Tensor):
     # V is of shape (num_batches, N, 2)
@@ -39,11 +42,14 @@ def apply_order_parameters(indices: torch.Tensor, order_parameters: torch.Tensor
     # For s-wave, the multiplication is always with jsigma2 = [[0, 1], [-1, 0]] which
     # represent a simple shift
 
-    # Instead of matrix multiplication, just perturb the rows
+    # order parameters must be order_parameters = torch.concat([deltas, -deltas.conj()])
+
+    # Instead of matrix multiplication, just permute the rows
     row = indices[:, 0]
     col = indices[:, 1]
-    V = V[:, col, ]
-    Vmult = torch.stack([V[..., 1], -V[..., 0]], dim=-1)
+    res = torch.zeros_like(V)
+    res[:, row, :] = order_parameters.unsqueeze(-1).unsqueeze(0) * torch.stack([V[:, col, 1], -V[:, col, 0]], dim=-1)
+    return res
 
 
 
@@ -54,6 +60,26 @@ def main():
         [1, 0, 0],
         [1, 1, 1]
     ], dtype=torch.long)
+
+    order_params = torch.tensor([1])
+    order_params = torch.concat([order_params, -order_params.conj()])
+
+    indices = torch.tensor([
+        [1, 1],
+        # [1, 1]
+    ])
+
+    rowcol = bdg_order_matrix_indices(indices)
+
+
+    shape = (1, 4, 2)
+    vec = torch.randn(size=shape).to(torch.complex128)
+    res = apply_order_parameters(rowcol, order_params, vec)
+
+    print(res)
+    assert(False)
+
+
 
     blocks = torch.tensor([
         [
